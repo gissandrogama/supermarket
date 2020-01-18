a4f426c88a700437aae5c40d808555599fc78785
gissandrogama <gissandrogama@gmail.com>
2020-01-18 14:51:07 -0300

    migration compras users_providers and users_cotrib

---
+++ priv/repo/migrations/20200118174832_compras.exs
@@ -0,0 +1,7 @@
+defmodule SuperFast.Repo.Migrations.Compras do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
---
+++ priv/repo/migrations/20200118174839_frete.exs
@@ -0,0 +1,7 @@
+defmodule SuperFast.Repo.Migrations.Frete do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
